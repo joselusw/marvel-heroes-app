3870d377763ae1c59b2b295d1f9024388accd3fb
Joselu <jose.gallardo994@gmail.com>
2020-03-11 20:09:45 +0100

    maintenance: Improved splash screen

--- app/src/main/java/com/jsw/marvelheroes/Activitiy/SplashActivity.kt
+++ app/src/main/java/com/jsw/marvelheroes/Activitiy/SplashActivity.kt
@@ -8,7 +8,23 @@ import com.jsw.marvelheroes.Activitiy.HomeActivity
 import com.jsw.marvelheroes.R
 
 class SplashActivity : AppCompatActivity() {
+    private val splashTime = 2000L // 3 seconds
+    private lateinit var myHandler : Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        myHandler = Handler()
+
+        myHandler.postDelayed({
+            goToMainActivity()
+        },splashTime)
+    }
+
+    private fun goToMainActivity(){
+        val mainActivityIntent = Intent(applicationContext, HomeActivity::class.java)
+        startActivity(mainActivityIntent)
+        finish()
     }
 }
